9536a561c6225552a29858445c8a3d9b911104f1
azharAltaweel <azharabdalmajeed@gmail.com>
2026-04-21 00:37:46 +0300

    opp1

---
+++ Function2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function2
+{
+    internal class Program
+    {
+        //1) Create a function that takes two dates and returns the number of days between the first and second date.
+         static int getDays( DateTime d1, DateTime d2)
+        {
+            TimeSpan diff = d2 - d1;
+
+            return Math.Abs(diff.Days);
+        }
+
+        //2) Create a function that takes an array of strings and returns an array with only the strings that have numbers in them.If there are no strings containing numbers, return an empty array.
+        static string[] StringsWithNumber(string[] str)
+        {
+            List<string> result = new List<string>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < str[i].Length; j++)
+                {
+                    if (char.IsDigit(str[i][j]))
+                    {
+                        result.Add(str[i]);
+                        break;
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+
+        //3) Given a string, reverse all the words which have odd length.The even length words are not changed.
+        static string ReverseOddString(string str) {
+            if (str.Length % 2 != 0)
+            {
+                for
+            }
+        }
+
+
+        //4) A pandigital number contains all digits(0-9) at least once.Write a function that takes an integer, returning true if the integer is pandigital, and false otherwise.
+
+
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ OOP in C# Tasks1/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_in_C__Tasks1
+{
+    //-------------------------------Task1-------------------------------
+    class Person
+    {
+        public string Name { get; set; }
+        private int Age;
+        public void setAge(int age) {  this.Age = age; }
+        public int getAge() { return Age; }
+
+        public void PrintPersonInfo()
+        {
+            Console.WriteLine("Person name: "+ Name +" "+" Age: "+ Age);
+        }
+    }
+    class Student: Person
+    {
+        private int Id;
+
+        public void setId(int id) { this.Id = id; }
+        public int getId() { return Id; }
+        public void PrintStdId()
+        {
+            Console.WriteLine("student id: "+Id+"\n");
+        }
+    }
+
+
+
+    //-------------------------------Task2-------------------------------
+    class Car
+    {
+        public string Make {  get; set; }
+        public int Year {  get; set; }
+        public string Type { get; set; }
+        public double   Price { get; set; }
+        public string Model { get; set; }
+        public string PalletNo { get; set; }
+        public string Color { get; set; }
+
+        private bool isEnginOn = false;
+        public Car(string m, int y, string t, double p, string mod, string pal, string c)
+        {
+            Make = m;
+            Year = y;
+            Type = t;
+            Price = p;
+            Model = mod;
+            PalletNo = pal;
+            Color = c;  
+        }
+         public void setEnginOn()
+        {
+            if (!isEnginOn)
+            {
+                isEnginOn = true;
+                Console.WriteLine("Engine started.\n");
+            }
+            else
+                Console.WriteLine("Engine is already running.\n");
+        }
+        public void setEnginOff()
+        {
+            if (isEnginOn)
+            {
+                isEnginOn = false;
+                Console.WriteLine("Engine stoped.\n");
+            }
+            else
+                Console.WriteLine("Engine is already off.\n");
+        }
+
+        public string GetCarInfo() {
+        return $"Make: {Make} | Model: {Model} | Year: {Year}\n" +
+           $"Type: {Type} | Price: ${Price}\n" +
+           $"Pallet No: {PalletNo} | Color: {Color}\n" +
+           $"Engine Status: {(isEnginOn ? "Running" : "Stopped")}\n";
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //-------------------------------Task1-------------------------------
+            Person p = new Person();
+            p.Name = "Azhar";
+            p.setAge(23);
+            p.PrintPersonInfo();
+
+            Student s1= new Student();
+            s1.setId(1);
+            s1.Name = "sara";
+            s1.setAge(25);
+            s1.PrintPersonInfo();
+            s1.PrintStdId();
+
+
+
+            //-------------------------------Task2-------------------------------
+            Car car1 = new Car("Toyota",2022,"Sedan",20000,"coralla","p12345","white");
+            Console.WriteLine(car1.GetCarInfo());
+            car1.setEnginOn();
+            Console.WriteLine(car1.GetCarInfo());
+
+
+        }
+    }
+}
